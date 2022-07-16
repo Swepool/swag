e45c7e96cac6c3b1afd241b7198fde5bbc709e58
Lukas <36674091+Swepool@users.noreply.github.com>
2022-07-16 23:25:07 +0200

    Push

--- src/routes/faq.svelte
+++ src/routes/faq.svelte
@@ -1,28 +1,52 @@
 <script>
-
+    import {Accordion, AccordionItem} from 'svelte-simple-accordion';
 </script>
 
 <div>
     <h1>Frequently asked questions</h1>
-    <div>
-        <h2>How much will shipping and taxes cost?</h2>
-        <p>Shipping and VATincluded!</p>
-        <p>Every price you see is the final price for that item, worldwide shipping and VAT included.</p>
-    </div>
-    <div>
-        <h2>How long does shipping take?</h2>
-        <p>Instead of having a bunch of items sitting in a warehouse, all items are produced when you order them. It takes 2 - 7 days for production and about 4 days for shipping, on average (but it mostly depends on what country you're shipping to).</p>
-        <p>In short: items will reach most destinations in 1 - 2 weeks.</p>
-    </div>
-    <div>
-        <h2>Are returns accepted?</h2>
-        <p>Unfortunately, no. Each item is made custom for each order, so returns can't be accepted.</p>
-    </div>
+    <Accordion>
+
+        <AccordionItem>
+            <div slot="title">
+                <h2>How much will shipping and taxes cost?</h2>
+            </div>
+            <div slot="content">
+                <p>Shipping and VATincluded!</p>
+                <p>Every price you see is the final price for that item, worldwide shipping and VAT included.</p>
+            </div>
+        </AccordionItem>
+
+        <AccordionItem>
+            <div slot="title">
+                <h2>Are returns accepted?</h2>
+            </div>
+            <div slot="content">
+                <p>Unfortunately, no. Each item is made custom for each order, so returns can't be accepted.</p>
+            </div>
+        </AccordionItem>
+
+        <AccordionItem>
+            <div slot="title">
+                <h2>How long does shipping take?</h2>
+            </div>
+            <div slot="content">
+                <p>Instead of having a bunch of items sitting in a warehouse, all items are produced when you order them. It takes 2 - 7 days for production and about 4 days for shipping, on average (but it mostly depends on what country you're shipping to).</p>
+                <p>In short: items will reach most destinations in 1 - 2 weeks.</p>
+            </div>
+        </AccordionItem>
+
+    </Accordion>
 </div>
 
 <style lang="scss">
    div {
      margin: 0 auto;
-     max-width: 600px;
+     width: 100%;
+     max-width: 1000px;
+     padding: 0 30px;
+
+     @media screen and (max-width: 568px) {
+       padding: 0 20px;
+     }
    }
 </style>
--- src/routes/success.svelte
+++ src/routes/success.svelte
@@ -12,6 +12,7 @@
         })
 
         cart.set([])
+
     })
 </script>
 
@@ -26,7 +27,7 @@
             {#each copyOfCart as item}
                 <div class="card">
                     <img src={item.image} alt="">
-                    <div style="display: flex; flex-direction: column">
+                    <div style="display: flex; justify-content: space-between; align-items: center; width: 100%">
                         <h3 style="margin: 0">{item.title}</h3>
                         <p style="margin: 0">{item.price} EUR</p>
                     </div>
@@ -37,36 +38,37 @@
 </div>
 
 <style lang="scss">
+
   .wrapper {
     display: grid;
-    grid-template-columns: repeat(2, minmax(0, 1fr));
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+    gap: 1rem;
     max-width: 1000px;
     width: 100%;
-    margin: 0 auto;
+    margin: 60px auto 0 auto;
     padding: 30px;
 
-    @media screen and (max-width: 568px) {
+    @media screen and (max-width: 768px) {
       padding: 20px;
-      grid-template-columns: repeat(1, minmax(0, 1fr));
+      grid-template-columns: repeat(1, minmax(0, 2fr));
     }
   }
 
   .message {
-    column-span:  span1/span1;
+    grid-column-start: 1;
+    grid-column:  span 1 / span 1;
   }
 
   .order {
-    column-span:  span1/span1;
+    width: 100%;
+    grid-column: span 2 /span 2;
     display: grid;
     gap: 1rem;
-    grid-template-columns: repeat(2, minmax(0, 1fr));
-
-    @media screen and (max-width: 868px) {
-      grid-template-columns: repeat(1, minmax(0, 1fr));
-    }
+    grid-template-columns: repeat(1, minmax(0, 1fr));
   }
 
   .card {
+    width: 100%;
     column-span: span1/span1;
     display: flex;
     gap: 20px;
